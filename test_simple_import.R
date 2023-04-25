e9e6c9c8c4a4ae7b0b170d81d28b352cef835d2d
Ghislain Durif <gd.dev@libertymail.net>
2023-04-25 16:42:24 +0200

    test is ok when loading R package before managing reticulate

--- test_simple_import.R
+++ test_simple_import.R
@@ -1,11 +1,14 @@
 # req
 library(reticulate)
 
+# import pandas
+pd <- import("pandas", as = "pd", delay_load = TRUE)
+
 # Python environement name
 venv <- "reticulate-test-env"
 
 # remove Python environment if existing
-virtualenv_remove(venv, confirm = FALSE)
+try(virtualenv_remove(venv, confirm = FALSE))
 
 # create Python environment
 virtualenv_create(venv)
@@ -19,8 +22,14 @@ py_config()
 # check if Pandas is available
 py_module_available("pandas")
 
-# import pandas
-pd <- import("pandas", as = "pd", delay_load = TRUE)
+# # import pandas
+# pd <- import("pandas", as = "pd", delay_load = TRUE)
+
+# install pandas
+virtualenv_install(venv, "pandas")
+
+# test
+pd$DataFrame(list('A' =  c(1, 2, 3)))
 
 # session info
 sessionInfo()
